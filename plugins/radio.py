688b470c08c253937cc1485f99122347de1e818a
muntazer <102398991+muntazer995@users.noreply.github.com>
2022-06-21 21:31:40 +0300

    Update radio.py

--- plugins/radio.py
+++ plugins/radio.py
@@ -23,18 +23,18 @@ from config import Config
 STREAM_URL=Config.STREAM_URL
 ADMINS=Config.ADMINS
 
-@Client.on_message(filters.command("radio") & filters.user(ADMINS))
+@Client.on_message(filters.command("صوت") & filters.user(ADMINS))
 async def radio(client, message: Message):
     if 1 in RADIO:
-        await message.reply_text(f"{emoji.ROBOT} **Please Stop Existing Radio Stream By /stopradio Command!**")
+        await message.reply_text(f"{emoji.ROBOT} **يرجى إيقاف تدفق الراديو الحالي عن طريق / توقف Command!**")
         return
     await mp.start_radio()
-    await message.reply_text(f"{emoji.CHECK_MARK_BUTTON} **Radio Stream Started :** \n<code>{STREAM_URL}</code>")
+    await message.reply_text(f"{emoji.CHECK_MARK_BUTTON} **بدأ دفق الراديو :** \n<code>{STREAM_URL}</code>")
 
-@Client.on_message(filters.command("stopradio") & filters.user(ADMINS))
+@Client.on_message(filters.command("توقف") & filters.user(ADMINS))
 async def stop(_, message: Message):
     if 0 in RADIO:
-        await message.reply_text(f"{emoji.ROBOT} **Please Start A Radio Stream First By /radio Command!**")
+        await message.reply_text(f"{emoji.ROBOT} **يرجى بدء تشغيل راديو أولاً بواسطة / أمر راديو!**")
         return
     await mp.stop_radio()
     await message.reply_text(f"{emoji.CROSS_MARK_BUTTON} **Radio Stream Ended Successfully!**")
